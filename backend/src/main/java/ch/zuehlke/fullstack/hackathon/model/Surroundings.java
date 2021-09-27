71b9adc65d74f117cab617d02beeb333921bfff4
Stalder, Pascal <pascal.stalder@zuehlke.com>
2021-09-27 16:21:59 +0200

    implement api

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Surroundings.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Surroundings.java
@@ -1,4 +1,4 @@
 package ch.zuehlke.fullstack.hackathon.model;
 
-public record Surroundings(FieldContent left, FieldContent right, FieldContent up, FieldContent down) {
+public record Surroundings(FieldType left, FieldType right, FieldType up, FieldType down) {
 }

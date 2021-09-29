41ceeb96e3de6a0e4a5922491380cd5065417e21
Stalder, Pascal <pascal.stalder@zuehlke.com>
2021-09-29 13:21:10 +0200

    fix typo

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/FieldObjects.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/FieldObjects.java
@@ -3,6 +3,6 @@ package ch.zuehlke.fullstack.hackathon.model;
 public class FieldObjects {
     public FieldType EMPTY = FieldType.EMPTY;
     public FieldType PLAYER = FieldType.PLAYER;
-    public FieldType BOARDER = FieldType.BORDER;
+    public FieldType BORDER = FieldType.BORDER;
     public FieldType FLAG = FieldType.FLAG;
 }
